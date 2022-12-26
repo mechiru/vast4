d91b69150c76ba07bff319b99ef0f32f0e97a21c
mechiru <u9053u6d41@gmail.com>
2022-12-27 08:17:26 +0900

    fix(tests): add VAST schema checker

---
+++ tests/checker/VastSchemaChecker/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Linq;
+using System.Xml.Schema;
+
+Console.WriteLine("Start VAST schema checker!");
+
+XNamespace ns = "http://www.iab.com/VAST";
+
+var schema = new XmlSchemaSet();
+schema.Add(ns.ToString(), "./vast_4.2.xsd");
+
+foreach (var file in Directory.GetFiles("../../output/v4_2"))
+{
+    Console.WriteLine($"Check '{file}'");
+    var xdoc = XDocument.Load("../../output/v4_2/Ad_Verification-test.xml");
+    xdoc.Root!.Name = ns + xdoc.Root!.Name.LocalName;
+    // Console.WriteLine($"Input XDocument: {xdoc}");
+
+    xdoc.Validate(schema, static (object? sender, ValidationEventArgs e) =>
+    {
+        // Console.WriteLine($"sender: {sender}");
+        // Console.WriteLine($"args: {e}");
+        Console.WriteLine($"Message: {e.Message}");
+    });
+}
+
+Console.WriteLine("Done!");
